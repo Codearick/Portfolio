48bcd1a801ed45830ba7d03e695df468dfd2db22
Codearick <rohan.gautam650@gmail.com>
2025-01-27 23:36:13 +0530

    add changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,10 +10,21 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      },
+      animation: {
+        'spin-slow': 'spin 7s linear infinite',     
+      },
+      backgroundImage: {
+        "circularLight" : 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px );'
+      }
     },
   },
   plugins: [],
